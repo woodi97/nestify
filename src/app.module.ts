da99315f8e95d319f8a23cc4315843468825d003
woodi <woodi.daily@gmail.com>
2022-01-22 01:53:47 +0900

    Add Auth & Boards Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+// import { MongooseModule } from '@nestjs/mongoose';
 
 import { MoviesModule } from './modules/movies/movies.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { BoardsModule } from './modules/boards/boards.module';
 
 @Module({
   imports: [
@@ -10,6 +13,13 @@ import { MoviesModule } from './modules/movies/movies.module';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'test' ? '.test.env' : '.env',
     }),
+    // MongooseModule.forRoot(process.env.MONGO_URI as string, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    // }),
+    AuthModule,
+    BoardsModule,
   ],
 })
 export class AppModule {}
